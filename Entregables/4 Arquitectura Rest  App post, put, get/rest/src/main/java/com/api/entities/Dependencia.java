069d9d1d163f4f9b1143ce6c28a7157569e528a4
David <Davidhernandezjuajinoy@gmail.com>
2021-01-30 13:12:09 -0500

    API[ENTITIES FULL]

--- Entregables/4 Arquitectura Rest  App post, put, get/rest/src/main/java/com/api/entities/Dependencia.java
+++ Entregables/4 Arquitectura Rest  App post, put, get/rest/src/main/java/com/api/entities/Dependencia.java
@@ -6,10 +6,83 @@
 
 package com.api.entities;
 
+
+
 /**
  *
  * @author David Juajinoy
  */
-public class Dependencia {
 
+public class Dependencia {
+    private int id;
+    private String codigo;
+    private String descripcion;
+    private String cargo;
+    private boolean estado;
+
+    public Dependencia() {
+    }
+
+    public Dependencia(int id, String codigo, String descripcion, String cargo, boolean estado) {
+	this.id = id;
+	this.codigo = codigo;
+	this.descripcion = descripcion;
+	this.cargo = cargo;
+	this.estado = estado;
+    }
+
+    
+    public int getId() {
+	return id;
+    }
+
+    public void setId(int id) {
+	this.id = id;
+    }
+
+    public String getCodigo() {
+	return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+	this.codigo = codigo;
+    }
+
+    public String getDescripcion() {
+	return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+	this.descripcion = descripcion;
+    }
+
+    public String getCargo() {
+	return cargo;
+    }
+
+    public void setCargo(String cargo) {
+	this.cargo = cargo;
+    }
+
+    public boolean getEstado() {
+	return estado;
+    }
+
+    public void setEstado(boolean estado) {
+	this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+	return "Dependencia{" + "id=" + id + ", codigo=" + codigo + ", descripcion=" + descripcion + ", cargo=" + cargo + ", estado=" + estado + '}';
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
--- Entregables/4 Arquitectura Rest  App post, put, get/rest/src/main/java/com/api/entities/Rol.java
+++ Entregables/4 Arquitectura Rest  App post, put, get/rest/src/main/java/com/api/entities/Rol.java
@@ -6,8 +6,13 @@
 
 package com.api.entities;
 
+
 import java.io.Serializable;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
 /**
@@ -15,16 +20,72 @@ import javax.persistence.Table;
  * @author David Juajinoy
  */
 
-
 @Entity
-@Table( name = "ROL" )
+@Table( name = "ROL")
 
-public class Rol  implements Serializable {
-    
+public class Rol implements Serializable {
+
     private static final long serialVersionUID = -6362794385792247263L;
     private int id;
     private String descripcion;
     private String siglaRol;
-    private boolean estada;
+    private boolean estado;
+
+    public Rol() {
+    }
+
+    public Rol(int id, String descripcion, String siglaRol, boolean estado) {
+	this.id = id;
+	this.descripcion = descripcion;
+	this.siglaRol = siglaRol;
+	this.estado = estado;
+    }
+
+    @Id
+    @GeneratedValue( strategy = GenerationType.IDENTITY )
+    @Column( name = "ID_ROL_PK", nullable = false )
+
+    public int getId( ) {
+	    return id;
+    }
+
+    public void setId(int idRol) {
+	this.id = idRol;
+    }
+
+    @Column(name = "DESCRIPCION", nullable = false)
+
+    public String getDescripcion() {
+	return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+	this.descripcion = descripcion;
+    }
+
+    @Column(name = "SIGLA_ROL")
+
+    public String getSiglaRol() {
+	return siglaRol;
+    }
+
+    public void setSiglaRol(String siglaRol) {
+	this.siglaRol = siglaRol;
+    }
+
+    @Column(name = "ESTADO", nullable = false)
+    public boolean getEstado() {
+	return estado;
+    }
+
+    public void setEstado(boolean estado) {
+	this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+	return "Rol{" + "id=" + id + ", descripcion=" + descripcion + ", siglaRol=" + siglaRol + ", estado=" + estado + '}';
+    }
+    
 
 }
